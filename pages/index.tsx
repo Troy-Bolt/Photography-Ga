04673487a6889bea831ebe8aa1c23ffdd8429c35
Troy <troy@gigrilla.com>
2022-06-14 23:05:53 +0100

    Rows of Three

--- pages/index.tsx
+++ pages/index.tsx
@@ -41,7 +41,7 @@ export default function Gallery({ images }: { images: Image[] }) {
     <Navbar/>
     <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24
     sm:px-6 lg:max-w-7xl lg:px-8">
-      <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2
+      <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-3
       gap-x-6 lg:grid-cols-3 xl:grid-cols-2 xl:gap-x-8">
         {images.map((image) => (
           <BlurImage key={image.id} image={image} />
